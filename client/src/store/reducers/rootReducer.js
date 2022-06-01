05a7bb97e463674239eb5b30d4082fe81eb6ae66
Gray.1991 <Sergey.sp.1991@gmail.com>
2022-06-01 20:45:41 +0300

    cardsReducer

--- client/src/store/reducers/rootReducer.js
+++ client/src/store/reducers/rootReducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import { listsReducer } from "./listsReducer";
+import { cardsReducer } from "./cardsReducer";
 
 export const rootReducer = combineReducers({
   lists: listsReducer,
+  cards: cardsReducer,
 });
